552d1d69c21e311d265e15c57a250cee479fcfb2
cavin6080 <cavinmacwan1@gmail.com>
2023-02-25 18:39:10 +0530

    [FIX] Adding comments

--- lib/sealed_class_module/bloc/home_state.dart
+++ lib/sealed_class_module/bloc/home_state.dart
@@ -11,13 +11,19 @@ class _HomeLoaded {
 
 class _HomeError {}
 
+// You have to specify the possible types of HomeState here
+// This package supports upto 9 types
 class HomeState
     extends Union4Impl<_HomeInitial, _HomeLoading, _HomeLoaded, _HomeError> {
+  // This factory is used to instantiating individual "subclasses"
   static const factory =
       Quartet<_HomeInitial, _HomeLoading, _HomeLoaded, _HomeError>();
+
+  // A PRIVATE constructor which takes in the individual HomeStates
   HomeState._(Union4<_HomeInitial, _HomeLoading, _HomeLoaded, _HomeError> union)
       : super(union);
 
+  // PUBLIC factories constructors which hides the complexity from outside classes
   factory HomeState.initial() => HomeState._(factory.first(_HomeInitial()));
   factory HomeState.loading() => HomeState._(factory.second(_HomeLoading()));
   factory HomeState.loaded(PersonModel model) =>
